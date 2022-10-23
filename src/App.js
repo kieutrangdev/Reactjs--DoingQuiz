fe3faeca0973940f508fd7d37137aa02afb4a006
kieutrangdev <104716412+kieutrangdev@users.noreply.github.com>
2022-10-23 15:43:01 +0700

    before project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.scss';
+import MyComponent from './components/MyComponent';
+import { Button } from 'bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      hello
+     <button>h</button>
+
+    </div>
+  );
+}
+
+export default App;
